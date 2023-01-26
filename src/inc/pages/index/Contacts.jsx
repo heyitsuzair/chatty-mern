f4080ede4da3de90ef9a3cecced650ef8f512905
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-01-26 05:53:17 +0500

    Fixes

--- src/inc/pages/index/Contacts.jsx
+++ src/inc/pages/index/Contacts.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  PlainButton,
-  Text2Xl,
-  TextLg,
-  TextMd,
-  TextSm,
-} from "../../components/commons";
+import { PlainButton, Text2Xl } from "../../components/commons";
 import ContactItem from "./ContactItem";
 
 const Contacts = () => {
--- src/inc/pages/index/Home.jsx
+++ src/inc/pages/index/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
     <div className="h-screen p-5">
       <div className="bg-white rounded-xl shadow-md dark:border h-[94vh] dark:bg-gray-800 dark:border-gray-700">
         <div className="grid grid-cols-12">
-          <div className="col-span-4 border-r border-gray-800 dark:border-gray-500">
+          <div className="col-span-12 lg:col-span-4 border-r border-gray-800 dark:border-gray-500">
             <Contacts />
           </div>
-          <div className="col-span-8">Hello</div>
+          <div className="col-span-12 lg:col-span-8">Hello</div>
         </div>
       </div>
     </div>
